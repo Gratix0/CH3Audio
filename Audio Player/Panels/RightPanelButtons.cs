a0b7895e47719273bb437275fd14382f5d03f23c
Gratix0 <102300824+Gratix0@users.noreply.github.com>
2023-04-09 14:52:30 +0300

    LastChange

---
+++ Audio Player/Panels/RightPanelButtons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Audio_Player
+{
+
+    public partial class MainWindow : Window
+    {
+
+        private void PlayList_Click(object sender, RoutedEventArgs e)
+        {
+            SetVisibleMain();
+        }
+
+        private void AddToFolder_Click(object sender, RoutedEventArgs e)
+        {
+            SetVisibleFolderAdder();
+        }
+    }
+    
+}
